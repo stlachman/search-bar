abc9b3a60961be276e94ffe903fb3a90720a0601
Seth Lachman <lachmanseth17@gmail.com>
2019-10-08 14:05:56 -0400

    Created initial react app.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" name="search" placeholder="search here" />
+    </div>
+  );
+};
+
+export default SearchBar;
